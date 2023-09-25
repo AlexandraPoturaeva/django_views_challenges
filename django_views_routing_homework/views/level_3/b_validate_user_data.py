38e013a7db5f7bccf7c92792fc078e5f7ea892c6
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-25 12:29:04 +0300

    level 3, ex. b

--- django_views_routing_homework/views/level_3/b_validate_user_data.py
+++ django_views_routing_homework/views/level_3/b_validate_user_data.py
@@ -17,9 +17,80 @@
 Для тестирования рекомендую использовать Postman.
 Когда будете писать код, не забывайте о читаемости, поддерживаемости и модульности.
 """
-
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.http import HttpResponse, HttpRequest
+from typing import Mapping
+import json
+
+
+def full_name_is_valid(full_name: str) -> bool:
+    return 5 <= len(full_name) <= 256
+
+
+def email_is_valid(email: str) -> bool:
+    try:
+        validate_email(email)
+    except ValidationError:
+        return False
+
+    return True
+
+
+def registered_from_is_valid(registered_from: str) -> bool:
+    return registered_from in ('website', 'mobile_app')
+
+
+def age_is_valid(age: str) -> bool:
+    try:
+        int(age)
+    except ValueError:
+        return False
+
+    return True
+
+
+def user_data_is_valid(user_data: Mapping) -> bool:
+    user_data_keys = set(user_data.keys())
+
+    if user_data_keys.issubset({'full_name', 'email', 'registered_from', 'age'}):
+
+        full_name = user_data.get('full_name')
+        email = user_data.get('email')
+        registered_from = user_data.get('registered_from')
+        age = user_data.get('age')
+
+        if all([full_name, email, registered_from]):
+
+            validation_result = [
+                full_name_is_valid(full_name),
+                email_is_valid(email),
+                registered_from_is_valid(registered_from),
+            ]
+
+            if age:
+                validation_result.append(age_is_valid(age))
+
+            return all(validation_result)
+
+    else:
+        return False
 
 
 def validate_user_data_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    user_data = None
+    status = 200
+    content = ''
+
+    try:
+        user_data = json.loads(request.body)
+    except ValueError:
+        status = 400
+
+    if user_data:
+        if user_data_is_valid(user_data):
+            content = '{"is_valid": true}'
+        else:
+            content = '{"is_valid": false}'
+
+    return HttpResponse(content=content, status=status)
